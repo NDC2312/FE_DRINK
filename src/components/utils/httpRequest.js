db0d889544d14c9687edb9977743e8d1b2cc228e
tran nhat nam <tnn231223@gmail.com>
2025-05-14 12:51:53 +0700

    done

--- src/components/utils/httpRequest.js
+++ src/components/utils/httpRequest.js
@@ -3,7 +3,7 @@ import cookie from 'react-cookies';
 
 const httpRequest = axios.create({
     // baseURL: 'http://localhost:8000/api/v1/',
-    baseURL: 'https://ndev-backend.vercel.app/api/v1/',
+    baseURL: 'https://be-drink.vercel.app/api/v1/',
     headers: {
         'Content-Type': 'application/json',
     },
