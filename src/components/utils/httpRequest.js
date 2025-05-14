3db3f6c753debbc3077292af584592668f2a7050
tran nhat nam <tnn231223@gmail.com>
2025-05-14 10:34:06 +0700

    a

--- src/components/utils/httpRequest.js
+++ src/components/utils/httpRequest.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import cookie from 'react-cookies';
 
 const httpRequest = axios.create({
-    // baseURL: 'http://localhost:8000/api/v1/',
-    baseURL: 'https://ndev-backend.vercel.app/api/v1/',
+    baseURL: 'http://localhost:8000/api/v1/',
+    // baseURL: 'https://ndev-backend.vercel.app/api/v1/',
     headers: {
         'Content-Type': 'application/json',
     },
